340da6b06291fdbb7d9f78aa41cfb545d0b4052d
yoshi-air <ywatanab@mail.ccsf.edu>
2020-02-28 02:20:24 +0900

    add audio seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Audio.create!(name: '猫', file: open("#{Rails.root}/db/fixtures/CRAZY_KEN_BAND_猫.mp3"))
